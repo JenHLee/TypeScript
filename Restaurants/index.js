8a74c217dcdff00dc9b16eef8b10e2da3a090e45
JenHLee <hj.jennie.lee@gmail.com>
2022-07-26 01:02:58 -0600

    create PixelSmile

--- Restaurants/index.js
+++ Restaurants/index.js
@@ -2,36 +2,30 @@
 exports.__esModule = true;
 var restaurants_1 = require("./restaurants");
 var hour = new Date().getHours();
-var dollarSigns = "$$";
-var deliveryTimeMax = "90";
+var dollarSigns = '$$';
+var deliveryTimeMax = '90';
 var maxDistance = 10;
 var result;
 var priceBracket = dollarSigns.length;
-var filteredRestaurants = restaurants_1["default"].filter(function (
-  restaurant
-) {
-  if (restaurant.priceBracket > priceBracket) {
-    return false;
-  }
-  if (restaurant.deliveryTimeMinutes > deliveryTimeMax) {
-    return false;
-  }
-  if (restaurant.distance > maxDistance) {
-    return false;
-  }
-  if (
-    hour < Number(restaurant.openHour) ||
-    hour > Number(restaurant.closeHour)
-  ) {
-    return false;
-  }
-  return restaurant;
+var filteredRestaurants = restaurants_1["default"].filter(function (restaurant) {
+    if (restaurant.priceBracket > priceBracket) {
+        return false;
+    }
+    if (restaurant.deliveryTimeMinutes > deliveryTimeMax) {
+        return false;
+    }
+    if (restaurant.distance > maxDistance) {
+        return false;
+    }
+    if (hour < Number(restaurant.openHour) || hour > Number(restaurant.closeHour)) {
+        return false;
+    }
+    return restaurant;
 });
 if (filteredRestaurants.length === 0) {
-  result = "There are no restaurants available right now.";
-} else {
-  result = "We found "
-    .concat(filteredRestaurants.length, " restaurants, the first is ")
-    .concat(filteredRestaurants[0].name, ".");
+    result = 'There are no restaurants available right now.';
 }
-console.log(result);
+else {
+    result = "We found ".concat(filteredRestaurants.length, " restaurants, the first is ").concat(filteredRestaurants[0].name, ".");
+}
+//console.log(result);
